17f5afa4d27d1b103bdcb15c416667757a2c2a63
dearahayunuraeni <dearahayu270@gmail.com>
2025-02-20 15:09:56 +0700

    praktikum01

---
+++ variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinisikan variable
+$nama = 'Dea' ;
+$umur = 20 ;
+$berat = 45.0 ;
+
+echo 'Nama saya : ' . $nama;
+echo '<br/>Umur : ' . $umur . ' Tahun';
+echo '<br/>Berat : ' . $berat;
+
+?>
